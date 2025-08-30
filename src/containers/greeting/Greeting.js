9728d963fc0fd48f7a9eefa530e42bec2b42f2f5
beatrizabadj <beatrizabadjim@gmail.com>
2025-08-30 16:29:20 +0200

    update cv

--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -42,7 +42,7 @@ export default function Greeting() {
                 <Button text="Contact me" href="#contact" />
                 {greeting.resumeLink && (
                   <a
-                    href={require("./Abad Beatriz CV - Web Development .pdf")}
+                    href={require("./CV - Abad, Beatriz.pdf")}
                     download="Abad Beatriz CV - Web Development .pdf"
                     className="download-link-button"
                   >
